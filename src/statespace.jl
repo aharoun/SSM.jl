b820cb992ee76c15082c1b649c360f9f72577ad7
Harun Alp <harun.alp@gmail.com>
2019-01-01 15:20:49 -0500

    recover estimated params for arima

--- src/statespace.jl
+++ src/statespace.jl
@@ -50,25 +50,27 @@ Estimates state space model. All the entries of the state space matrices with Na
 """
 function estimate(ssm::StateSpace,y)
 
-    estFNames, estFIndex, nParEst = getEstParamIndex(ssm::StateSpace)
+    estFNames, estFIndex, nParEst = getEstParamIndexSSM(ssm::StateSpace)
 
-    objFun = x -> ssmNegLogLike(x, ssm, y, estFNames, estFIndex)
-    pInit  = initializeCoeff(ssm.model,y,nParEst)
+    pInit = initializeCoeff(ssm.model,y,nParEst)
 
-    res    = optimize(x -> ssmNegLogLike(x, ssm, y, estFNames, estFIndex),
+    res   = optimize(x -> ssmNegLogLike!(x, ssm, y, estFNames, estFIndex),
 		      pInit,
 		      Optim.Options(g_tol = 1.0e-8, iterations = 1000, store_trace = false, show_trace = false))
 
-    return res,ssm
+    ssmNegLogLike!(res.minimizer, ssm, y, estFNames, estFIndex)
+    return ssm,res
 end
 
 function estimate(a::AbstractTimeModel,y)
-    ssm = StateSpace(a)
-    estimate(ssm::StateSpace,y)
+    ssm     = StateSpace(a)
+    ssm,res = estimate(ssm::StateSpace,y)
+    getParamFromSSM!(ssm,a)
+    return a, res
 end
 
 
-function getEstParamIndex(ssm::StateSpace)
+function getEstParamIndexSSM(ssm::StateSpace)
     # mask parameters to be estimated
     indx = _findEstParamIndex(ssm)
 
@@ -83,8 +85,14 @@ function getEstParamIndex(ssm::StateSpace)
 end
 
 
+function getParamFromSSM!(ssm::StateSpace, a::arima)
+     a.ϕ .= ssm.G[1+a.d:a.p+a.d,1 + a.d]
+     a.θ .= ssm.R[2+a.d:1+a.q+a.d]
+     a.σ2 = ssm.S[1]
+end
+
 # objective function for `estimate`
-function ssmNegLogLike(x,ssm::StateSpace, y, estFNames, estFIndex)
+function ssmNegLogLike!(x,ssm::StateSpace, y, estFNames, estFIndex)
     count = 1
     @inbounds for (i,valF) in enumerate(estFNames)
       for j in eachindex(estFIndex[i])
